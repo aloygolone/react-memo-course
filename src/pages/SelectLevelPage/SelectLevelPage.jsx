3a43daba57e9d84e14bed9677957ca70e3e17af5
Eugene Alyoshin <aloyolreal@gmail.com>
2024-03-29 19:57:49 +0700

    пока так

--- src/pages/SelectLevelPage/SelectLevelPage.jsx
+++ src/pages/SelectLevelPage/SelectLevelPage.jsx
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom"
 import styles from "./SelectLevelPage.module.css"
 import Checkbox from "../../components/Checkbox/Checkbox"
+import { useGameMode } from "../../hooks/useGameMode"
 
 export function SelectLevelPage() {
+  const { isSelected, setIsSelected } = useGameMode()
+  const selectGameMode = () => {
+    setIsSelected(prevState => !prevState)
+    console.log(isSelected)
+  }
   return (
     <div className={styles.container}>
       <div className={styles.modal}>
@@ -28,7 +34,7 @@ export function SelectLevelPage() {
         {/* 2. отобразить количество попыток в компоненте cards, там же будет состояние попыток */}
         {/* 3. нужно в cards в OpenCard мы должны создать условие если включен режим */}
         {/* 4.  если ровно 2 карточки без пары, а попытки еще остались то мы должны перевернуть карточки без пары и вычесть попытку*/}
-        <Checkbox id={"modeCheckbox"} name={"modeCheckbox"} label={"Игра до 3 ошибок"} onClick={() => {}} />
+        <Checkbox id={"modeCheckbox"} name={"modeCheckbox"} label={"Игра до 3 ошибок"} onClick={selectGameMode()} />
       </div>
     </div>
   )
